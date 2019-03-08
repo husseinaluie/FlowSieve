0832131df4d693ad4f08be884bc0a263198f8a65
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2019-03-08 12:59:40 -0500

    Adding perctile options for upper bound in signed log plot.
    This is useful for noisy data, since the max may me inappropriate.

--- PythonTools/PlotTools/SignedLogPlot_onMap.py
+++ PythonTools/PlotTools/SignedLogPlot_onMap.py
@@ -6,7 +6,7 @@ import numpy as np
 def SignedLogPlot_onMap(XX, YY, ZZ, ax, fig, proj_map,
         num_ords=6, xscale = 'linear', yscale = 'linear',
         xlabel = '', ylabel = '', title = '',
-        latlon=True):
+        latlon=True, percentile=100):
     # figsize:  size of the figure (passed to figsize flag in matplotlib)
     # num_ords: number of orders of magnitude to show on colour bar
     # dpi:      resolution of output file
@@ -30,7 +30,7 @@ def SignedLogPlot_onMap(XX, YY, ZZ, ax, fig, proj_map,
     blue_map.set_under('w')
 
     ## Plot
-    vmax = np.max(np.abs(ZZ))
+    vmax = np.percentile(np.abs(ZZ), percentile)
     vmin = 10**(np.ceil(np.log10(vmax)-num_ords)-1.0)
 
     # Plot the positives in red
@@ -69,11 +69,15 @@ def SignedLogPlot_onMap(XX, YY, ZZ, ax, fig, proj_map,
     cax2 = fig.add_axes([x0, y0,               0.05*width, height*0.475])
 
     # Colour bars
-    cbar1 = plt.colorbar(q1, cax=cax1, extend='min')
+    if (percentile < 100):
+        extend = 'both'
+    else:
+        extend='min'
+    cbar1 = plt.colorbar(q1, cax=cax1, extend=extend)
     cbar1.solids.set_rasterized(True)
     cbar1.solids.set_edgecolor("face")
 
-    cbar2 = plt.colorbar(q2, cax=cax2, extend='min')
+    cbar2 = plt.colorbar(q2, cax=cax2, extend=extend)
     cbar2.solids.set_rasterized(True)
     cbar2.solids.set_edgecolor("face")
 
