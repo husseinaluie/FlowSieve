3ba57775c2d7a8a47d0fedbb80aa6b118db090f5
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2019-03-20 16:05:39 -0400

    Updating interpolator to use new IO routines

--- interpolator.cpp
+++ interpolator.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "netcdf_io.hpp"
 #include "preprocess.hpp"
+#include "constants.hpp"
 
 #ifndef DEBUG
     #define DEBUG 0
@@ -55,7 +56,7 @@ int main(int argc, char **argv)
     fprintf(stdout, "Interpolating potential temperature (thetao)\n");
     #endif
     interpolate_over_land(thetao_interp, thetao, time, depth, latitude, longitude, mask);
-
+    
     #if DEBUG >= 0
     fprintf(stdout, "Interpolating salinity (so)\n");
     #endif
@@ -65,7 +66,7 @@ int main(int argc, char **argv)
     fprintf(stdout, "Interpolating sea surface height (zos)\n");
     #endif
     interpolate_over_land(ssh_interp, ssh, time, depth, latitude, longitude, mask);
-
+    
     #if DEBUG >= 0
     fprintf(stdout, "Creating output\n");
     #endif
@@ -75,13 +76,13 @@ int main(int argc, char **argv)
     fprintf(stdout, "Adding appropraite variables to source.nc\n");
     #endif
     const char* dim_names[] = {"time", "depth", "latitude", "longitude"};
-    add_var_to_file("thetao_orig", "interp.nc", dim_names, 4);
-    add_var_to_file("sal_orig",    "interp.nc", dim_names, 4);
-    add_var_to_file("ssh_orig",    "interp.nc", dim_names, 4);
+    add_var_to_file("thetao_orig", dim_names, 4, "interp.nc");
+    add_var_to_file("sal_orig",    dim_names, 4, "interp.nc");
+    add_var_to_file("ssh_orig",    dim_names, 4, "interp.nc");
 
-    add_var_to_file("thetao_interp", "interp.nc", dim_names, 4);
-    add_var_to_file("sal_interp",    "interp.nc", dim_names, 4);
-    add_var_to_file("ssh_interp",    "interp.nc", dim_names, 4);
+    add_var_to_file("thetao_interp", dim_names, 4, "interp.nc");
+    add_var_to_file("sal_interp",    dim_names, 4, "interp.nc");
+    add_var_to_file("ssh_interp",    dim_names, 4, "interp.nc");
 
     size_t starts[] = {0, 0, 0, 0};
     size_t counts[] = {time.size(),
@@ -100,6 +101,33 @@ int main(int argc, char **argv)
     write_field_to_output(thetao_interp, "thetao_interp", starts, counts, "interp.nc");
     write_field_to_output(sal_interp,    "sal_interp",    starts, counts, "interp.nc");
     write_field_to_output(ssh_interp,    "ssh_interp",    starts, counts, "interp.nc");
+    
+
+    /*
+    // Now that we've interpolated them, let's compute the density and pressure at each point
+    std::vector<double> pressure(sal.size());
+    std::vector<double> density(sal.size());
+
+    double temp;
+    for (size_t II = 0; II < sal.size();, II++) {
+        // Compute pressure (geostrophic)
+        pressure.at(II) = constants::rho0 * constants::g * ssh_interp.at(II);
+
+        // Convert potential temp to actual temp
+        temp = depotential_temperature(pressure.at(II), thetao.at(II));
+
+        // Compute density from UNESCO equation of state
+        density.at(II) = equation_of_state(temp, 
+                sal_interp.at(II), 
+                pressure.at(II) / 1e5); // takes pressure in bar = 100 kPa = 1e5 Pa
+    }
+
+    add_var_to_file("pressure", dim_names, 4, "interp.nc");
+    add_var_to_file("density",  dim_names, 4, "interp.nc");
+
+    write_field_to_output(pressure, "pressure", starts, counts, "interp.nc");
+    write_field_to_output(density,  "density",  starts, counts, "interp.nc");
+    */
 
     return 0;
 }
