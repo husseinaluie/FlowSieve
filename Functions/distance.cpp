c0e8074a4370de04117c6d53df37f79c6f65d5c0
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2019-03-27 14:54:16 -0400

    Making distance Cartesian friendly. Again reducing total sin / cos calls

--- Functions/distance.cpp
+++ Functions/distance.cpp
@@ -2,18 +2,39 @@
 #include <math.h>    
 #include "../constants.hpp"
 
-#ifndef DEBUG
-    #define DEBUG 0
-#endif
-
 double distance(
-        const double lon1, /**< [in] Longitude of first position */
-        const double lat1, /**< [in] Latitude of first position */
-        const double lon2, /**< [in] Longitude of second position */
-        const double lat2  /**< [in] Latitude of second position */
+        const double lon1,  /**< [in] Longitude of first position */
+        const double lat1,  /**< [in] Latitude of first position */
+        const double lon2,  /**< [in] Longitude of second position */
+        const double lat2,  /**< [in] Latitude of second position */
+        const double Llon,  /**< [in] Physical length of the longitude (first) dimension */
+        const double Llat   /**< [in] Physical length of the latitude (second) dimension */
         ) {
 
-    double numer, denom, Delta_sigma, distance;
+    double distance;
+
+    #if CARTESIAN
+    // If we're on a Cartesian grid, then just compute the straight-forward
+    //   Cartesian distance. Account for periodicity if necessary
+
+    #if PERIODIC_X
+    const double del_x = fmin( fabs(lon1 - lon2), Llon - fabs(lon1 - lon2));
+    #else
+    const double del_x = lon1 - lon2;
+    #endif
+
+    #if PERIODIC_Y
+    const double del_y = fmin( fabs(lat1 - lat2), Llat - fabs(lat1 - lat2));
+    #else
+    const double del_y = lat1 - lat2;
+    #endif
+
+    distance = sqrt( pow(del_x, 2) + pow(del_y, 2) );
+
+    #elif not(CARTESIAN)
+    // If not on a Cartesian grid, then we're on a spherical grid.
+    //   Compute distances along great circles.
+    double numer, denom, Delta_sigma;
 
     const double Delta_lon = lon2 - lon1;
     const double cos_lat2 = cos(lat2);
@@ -22,14 +43,6 @@ double distance(
     const double sin_lat1 = sin(lat1);
     const double cos_Delta_lon = cos(Delta_lon);
 
-    /*
-    numer =   pow(                                              cos(lat2) * sin(Delta_lon), 2 ) 
-                   + pow( cos(lat1) * sin(lat2)  -  sin(lat1) * cos(lat2) * cos(Delta_lon), 2 );
-    numer =  sqrt(numer);
-                
-    denom =        sin(lat1) * sin(lat2)  +  cos(lat1) * cos(lat2) * cos(Delta_lon);
-    */
-    
     numer =   pow(                                    cos_lat2 * sin(Delta_lon), 2 ) 
             + pow( cos_lat1 * sin_lat2  -  sin_lat1 * cos_lat2 * cos_Delta_lon , 2 );
     numer =  sqrt(numer);
@@ -39,6 +52,8 @@ double distance(
     Delta_sigma = atan2(numer, denom);
     
     distance = constants::R_earth * Delta_sigma;
+
+    #endif
 
     #if DEBUG >= 6
     fprintf(stdout, "dist(lon1 = %.5g, lat1 = %.5g, lon2 = %.5g, lat2 = %.5g) = %.5g\n",
