cd81e34c1d40e01fc1d5cb04ffa7748c0bd15eeb
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2019-04-05 15:02:27 -0400

    Adding other kernel options

--- Functions/kernel.cpp
+++ Functions/kernel.cpp
@@ -1,5 +1,6 @@
 #include "../functions.hpp"
 #include "../constants.hpp"
+#include <math.h>
 
 double kernel(
         const double dist,  /**< [in] Distance as argument to the kernel */
@@ -8,11 +9,17 @@ double kernel(
    
     double kern;
 
+    #if KERNEL_OPT == 0
     if ( dist < (scale / 2) ) {
         kern =  1.;
     } else {
         kern =  0.;
     }
+    #elif KERNEL_OPT == 1
+    kern = exp( -pow( dist / (scale/2)  , 4) );
+    #elif KERNEL_OPT == 2
+    kern = exp( -pow( dist / (scale/2)  , 2) );
+    #endif
 
     #if DEBUG >= 6
     fprintf(stdout, "Kernel(dist=%.4g, scale=%.4g) = %.4g\n",
