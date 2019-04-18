31142885512d44fc47fd66329c2cd620be173445
Benjamin Storer <bastorer@uwaterloo.ca>
2019-04-18 14:27:44 -0400

    Adding system file for Graham

---
+++ Systems/Graham.mk
@@ -0,0 +1,23 @@
+# Specify compilers
+CXX     ?= gcc
+MPICXX  ?= mpic++
+
+# Linking flags for netcdf
+LINKS:=-lnetcdf -lhdf5_hl -lhdf5 -lz -lcurl -fopenmp
+
+# Default compiler flags
+CFLAGS:=-Wall 
+
+# Debug flags
+DEBUG_FLAGS:=-g
+DEBUG_LDFLAGS:=-g
+
+# Basic optimization flags
+OPT_FLAGS:=-O3 #-fp-model fast=2
+
+# Extra optimization flags (intel inter-process optimizations)
+EXTRA_OPT_FLAGS:=#-ip -ipo
+
+# Modules are automatically on lib dir
+LIB_DIRS:=
+INC_DIRS:=
