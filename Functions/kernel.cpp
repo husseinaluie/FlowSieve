d3d810e186673ee99fc36a1e8bcc2934b76497ec
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2019-05-20 13:12:05 -0400

    Updating/tidying up kernel definitions

--- Functions/kernel.cpp
+++ Functions/kernel.cpp
@@ -3,13 +3,13 @@
 #include <math.h>
 
 double sinc(const double &x) {
-    if (x == 0.) {
-        return 1; // account for zero
-    } else if (fabs(x) < 1e-10) {
-        return 1 - x*x/6; // to avoid some numerical issues, use series expansion for small x
-    } else {
-        return sin(x) / x;
-    }
+    double s;
+
+    // to avoid some numerical issues, use series expansion for small x
+    if (fabs(x) < 1e-9) { s = 1. - x*x/6.; }
+    else                { s = sin(x) / x; }
+
+    return s;
 }
 
 double kernel(
@@ -18,16 +18,16 @@ double kernel(
         ) {
    
     double kern;
+    const double D = dist / ( scale / 2. );
 
     switch (constants::KERNEL_OPT) {
-        case 0: if ( dist < (scale / 2) ) { kern =  1.; }
-                else { kern =  0.; }
+        case 0: kern = dist < D ? 1 : 0;
                 break;
-        case 1: kern = exp( -pow( dist / (scale/2)  , 4) );
+        case 1: kern = exp( -pow( D, 4) );
                 break;
-        case 2: kern = exp( -pow( dist / (scale/2)  , 2) );
+        case 2: kern = exp( -pow( D, 2) );
                 break;
-        case 3: kern = sinc( dist / (scale/2) );
+        case 3: kern = sinc( M_PI * D );
                 break;
     }
 
