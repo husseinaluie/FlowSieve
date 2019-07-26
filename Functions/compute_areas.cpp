764e1438fe25c02d2b0e7c9c64230c9a714c7bd0
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2019-07-26 15:17:28 -0400

    Updating compute_area to handle non-uniform lat grid

--- Functions/compute_areas.cpp
+++ Functions/compute_areas.cpp
@@ -11,29 +11,66 @@ void compute_areas(
         const std::vector<double> &latitude     /**< [in] array containing latitude dimension (1D) */
         ) {
 
-    // For the moment, assume a uniform grid
-    const double dlat = latitude.at( 1) - latitude.at( 0);
-    const double dlon = longitude.at(1) - longitude.at(0);
+    double dlat, dlon, coeff;
+    int ii, jj;
 
     // Get the array sizes
     const int Nlon = longitude.size();
     const int Nlat = latitude.size();
 
-    double coeff;
-    if (constants::CARTESIAN) { coeff = dlat * dlon; }
-    else { coeff = pow( constants::R_earth, 2) * dlat * dlon; }
+    if (constants::UNIFORM_LAT_GRID) {
+        // For the moment, assume a uniform grid
+        dlat = latitude.at( 1) - latitude.at( 0);
+        dlon = longitude.at(1) - longitude.at(0);
 
-    // Compute the area of each cell
-    int ii, jj;
-    #pragma omp parallel default(none) private(ii, jj) shared(areas, coeff, latitude)
-    {
-        #pragma omp for collapse(2) schedule(static)
-        for (ii = 0; ii < Nlat; ii++) {
-            for (jj = 0; jj < Nlon; jj++) {
-                if (constants::CARTESIAN) {
-                    areas.at(ii*Nlon + jj) = coeff;
-                } else {
-                    areas.at(ii*Nlon + jj) = coeff * cos(latitude.at(ii));
+        if (constants::CARTESIAN) { coeff = dlat * dlon; }
+        else { coeff = pow( constants::R_earth, 2) * dlat * dlon; }
+
+        // Compute the area of each cell
+        #pragma omp parallel default(none) private(ii, jj) shared(areas, coeff, latitude)
+        {
+            #pragma omp for collapse(2) schedule(static)
+            for (ii = 0; ii < Nlat; ii++) {
+                for (jj = 0; jj < Nlon; jj++) {
+                    if (constants::CARTESIAN) {
+                        areas.at(ii*Nlon + jj) = coeff;
+                    } else {
+                        areas.at(ii*Nlon + jj) = coeff * cos(latitude.at(ii));
+                    }
+                }
+            }
+        }
+    } else {
+
+        // Compute the area of each cell
+        #pragma omp parallel default(none) private(ii, jj, dlat, dlon)\
+        shared(areas, coeff, longitude, latitude)
+        {
+            dlon = longitude.at(1) - longitude.at(0);
+            #pragma omp for collapse(2) schedule(static)
+            for (ii = 0; ii < Nlat; ii++) {
+                for (jj = 0; jj < Nlon; jj++) {
+
+                    // Get the lat grid spacing
+                    if (ii == 0) {
+                        dlat = ( latitude.at(1) - latitude.at(0) ) / 2.;
+                    } else if (ii == Nlat - 1) {
+                        dlat = ( latitude.at(Nlat-1) - latitude.at(Nlat-2) ) / 2.;
+                    } else {
+                        dlat =   ( latitude.at(ii) - latitude.at(ii-1) ) / 2.
+                               + ( latitude.at(ii) - latitude.at(ii-1) ) / 2.;
+                    }
+
+                    // Get coefficient
+                    if (constants::CARTESIAN) { coeff = dlat * dlon; }
+                    else { coeff = pow( constants::R_earth, 2) * dlat * dlon; }
+
+                    // Compute cell area
+                    if (constants::CARTESIAN) {
+                        areas.at(ii*Nlon + jj) = coeff;
+                    } else {
+                        areas.at(ii*Nlon + jj) = coeff * cos(latitude.at(ii));
+                    }
                 }
             }
         }
