3e7bfda01fd7da0fda704f5a158ca97ad0df203b
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2019-09-16 16:33:49 -0400

    Adding a Bluehive system file for openmpi and intel compilers

--- Systems/Bluehive_intel.mk
+++ Systems/Bluehive_intel.mk
@@ -1,15 +1,16 @@
 # The following modules were last used
-#  openmpi/2.0.1/b1  
-#  hdf5/1.8.19/b1     
-#  netcdf/4.3.3.1
-#  gcc/8.2.0/b1
+#  impi/2018
+#  intel/2018 (automatically loaded with impi/2018)
+#  netcdf/4.4.1.1/b1
+#  hdf5/1.8.17/b4  (automatically loaded with netcdf/4.4.1.1/b1)
+#  module unload impi/2017  (this is loaded with netcdf too, but we don't want it)
 
 # Specify compilers
-CXX     ?= g++ #icpc
-MPICXX  ?= mpicxx
+CXX     ?= icc
+MPICXX  ?= mpiicc
 
 # Linking flags for netcdf
-LINKS:=-lnetcdf -lhdf5_hl -lhdf5 -lz -lcurl -fopenmp
+LINKS:=-lnetcdf -lhdf5_hl -lhdf5 -lz -lcurl -qopenmp
 
 # Default compiler flags
 CFLAGS:=-Wall 
@@ -19,7 +20,7 @@ DEBUG_FLAGS:=-g
 DEBUG_LDFLAGS:=-g
 
 # Basic optimization flags
-OPT_FLAGS:=-O3 #-fp-model fast=2
+OPT_FLAGS:=-O3 -fp-model fast=2
 
 # Extra optimization flags (intel inter-process optimizations)
 EXTRA_OPT_FLAGS:=-ip -ipo
---
+++ Systems/Bluehive_open.mk
@@ -0,0 +1,30 @@
+# The following modules were last used
+#  openmpi/2.0.1/b1  
+#  hdf5/1.8.19/b1     
+#  netcdf/4.3.3.1
+#  gcc/8.2.0/b1
+#  fftw3/3.3.6/b1
+
+# Specify compilers
+CXX     ?= g++
+MPICXX  ?= mpicxx
+
+# Linking flags for netcdf
+LINKS:=-lnetcdf -lhdf5_hl -lhdf5 -lz -lcurl -fopenmp
+
+# Default compiler flags
+CFLAGS:=-Wall 
+
+# Debug flags
+DEBUG_FLAGS:=-g
+DEBUG_LDFLAGS:=-g
+
+# Basic optimization flags
+OPT_FLAGS:=-O3
+
+# Extra optimization flags (intel inter-process optimizations)
+EXTRA_OPT_FLAGS:=
+
+# Modules are automatically on lib dir
+LIB_DIRS:=
+INC_DIRS:=
