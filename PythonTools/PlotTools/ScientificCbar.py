3ca9641bc29b46864b31f3e5789f7563ba3d9f7f
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2020-01-13 13:14:13 -0500

    Adding some more functionality to ScientificCbar - centre val, etc

--- PythonTools/PlotTools/ScientificCbar.py
+++ PythonTools/PlotTools/ScientificCbar.py
@@ -1,13 +1,20 @@
 import matplotlib as mpl
 import numpy as np
+from matplotlib.colors import LogNorm
 
 def ScientificCbar(cbar, units='',
-        orientation='vertical', centre=True):
+        orientation='vertical', centre = True, centre_val=0.,
+        labelpad = 20, label='', logbar = False):
+
+    if ( logbar and centre ):
+        print("Cannot log a centred colour bar. Setting centre to False")
+        centre = False
 
     # If requested, centre the colour bar
     if centre:
-        cv = np.max(np.abs(cbar.mappable.get_clim()))
-        cbar.mappable.set_clim(-cv, cv)
+        cb_vals = cbar.mappable.get_clim()
+        cv = np.max(np.abs(np.array([val - centre_val for val in cb_vals])))
+        cbar.mappable.set_clim(centre_val-cv, centre_val+cv)
 
     # Limit the number of ticks on the colour bar
     tick_locator = mpl.ticker.MaxNLocator(nbins=5)
@@ -15,19 +22,31 @@ def ScientificCbar(cbar, units='',
     cbar.update_ticks()
     ticks = cbar.get_ticks()
 
-    # Re-scale the values to avoid the poorly-placed exponent
-    #   above the colour bar
-    scale = np.log10(np.max(np.abs(ticks)))
-    scale = np.floor(scale)
-
-    # Instead, simply add a ylabel to the colour bar giving the scale.
+    if logbar:
+        cbar.set_norm = LogNorm()
+        cb_label = units
+        if len(label) > 0:
+            cb_label = label + '\n' + cb_label
+    else:
+        # Re-scale the values to avoid the poorly-placed exponent
+        #   above the colour bar
+        scale = np.log10(np.max(np.abs(ticks)))
+        scale = np.floor(scale)
+    
+        # Label
+        cb_label = '$\\times10^{' + '{0:d}'.format(int(scale)) + '}$ ' + units
+        if len(label) > 0:
+            cb_label = label + '\n' + cb_label
+    
+        # Tick labels
+        tick_labels = ["{0:.2g}".format(tick/(10**scale)) for tick in ticks]
+    
+    # Add appropriate label to the bar
     if orientation == 'vertical':
         if scale != 0.:
-            cbar.ax.set_yticklabels(["{0:.2g}".format(tick/(10**scale)) for tick in ticks])
-        cbar.ax.set_ylabel('$\\times10^{' + '{0:d}'.format(int(scale)) + '}$ ' + units,
-                rotation = '-90', labelpad=10)
+            cbar.ax.set_yticklabels(tick_labels)
+        cbar.ax.set_ylabel(cb_label, rotation = '-90', labelpad = labelpad)
     elif orientation == 'horizontal':
         if scale != 0.:
-            cbar.ax.set_xticklabels(["{0:.2g}".format(tick/(10**scale)) for tick in ticks])
-        cbar.ax.set_xlabel('$\\times10^{' + '{0:d}'.format(int(scale)) + '}$ ' + units,
-                rotation = '0', labelpad=10)
+            cbar.ax.set_xticklabels(tick_labels)
+        cbar.ax.set_xlabel(cb_label, rotation = '0', labelpad = labelpad)
