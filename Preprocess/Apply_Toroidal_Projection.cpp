2d8f83ad675760a4bfe19fbaa2755f79446c4b12
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2020-02-06 15:45:51 -0500

    Adding in seeds and some diagnostics to the toroidal project driver
    Seeds can now be provided to (hopefully) speed up convergence.
    
    Either a single seed can be provided, in which case the solution
    from the previous Itime will be carried forward as the next seed,
    or a full seed set can be provided, so that each Itime has its own seed

--- Preprocess/Apply_Toroidal_Projection.cpp
+++ Preprocess/Apply_Toroidal_Projection.cpp
@@ -22,6 +22,8 @@ void Apply_Toroidal_Projection(
         const std::vector<double> & mask,
         const std::vector<int>    & myCounts,
         const std::vector<int>    & myStarts,
+        const std::vector<double> & seed,
+        const bool single_seed,
         const MPI_Comm comm
         ) {
 
@@ -29,8 +31,8 @@ void Apply_Toroidal_Projection(
     MPI_Comm_rank( comm, &wRank );
     MPI_Comm_size( comm, &wSize );
 
-    const double rel_tol   = 1e-5;
-    const int    max_iters = 1e5;
+    const double rel_tol   = 1e-7;
+    const int    max_iters = 1e4;
 
     // Create a 'no mask' mask variable
     //   we'll treat land values as zero velocity
@@ -55,15 +57,42 @@ void Apply_Toroidal_Projection(
         }
     }
 
+    // Get the divergence of the original reference field, for comparison
+    std::vector<double> full_div_orig(u_lon.size(), 0.);
+    toroidal_vel_div(full_div_orig, u_lon, u_lat, longitude, latitude,
+            Ntime, Ndepth, Nlat, Nlon, unmask);
+
     // Storage vectors
     std::vector<double> full_F(u_lon.size(), 0.),
+        full_Lap_F(u_lon.size(), 0.),
         full_u_lon_tor(u_lon.size(), 0.),
         full_u_lat_tor(u_lon.size(), 0.),
-        full_curl_term(u_lon.size(), 0.);
+        full_u_lon_seed(u_lon.size(), 0.),
+        full_u_lat_seed(u_lon.size(), 0.),
+        full_div_tor(u_lon.size(), 0.),
+        full_curl_term(u_lon.size(), 0.),
+        full_seed(u_lon.size(), 0.);
 
     // alglib variables
     alglib::real_1d_array rhs;
-    std::vector<double> curl_term(Npts, 0.);
+    std::vector<double> 
+        curl_term( Npts, 0.), 
+        Lap_F_tor( Npts, 0.),
+        F_seed(    Npts, 0.),
+        F_seed_Lap(Npts, 0.);
+    
+
+    // Copy the starting seed.
+    if (single_seed) {
+        for (Ilat = 0; Ilat < Nlat; ++Ilat) {
+            for (Ilon = 0; Ilon < Nlon; ++Ilon) {
+                index = Index(0, 0, Ilat, Ilon,
+                              1, 1, Nlat, Nlon);
+                F_seed.at(index) = seed.at(index);
+            }
+        }
+    }
+
     rhs.attach_to_ptr(Npts, &curl_term[0]);
 
     alglib::linlsqrstate state;
@@ -76,8 +105,10 @@ void Apply_Toroidal_Projection(
     //
     //// Build the LHS part of the problem (Lap)
     //
-    fprintf(stdout, "Building the LHS of the least squares problem.\n");
-    fflush(stdout);
+    if (wRank == 0) {
+        fprintf(stdout, "Building the LHS of the least squares problem.\n");
+        fflush(stdout);
+    }
 
     alglib::sparsematrix Lap;
     alglib::sparsecreate(Npts, Npts, Lap);
@@ -85,8 +116,10 @@ void Apply_Toroidal_Projection(
     toroidal_sparse_Lap(Lap, latitude, longitude, Nlat, Nlon, unmask);
     alglib::sparseconverttocrs(Lap);
 
-    fprintf(stdout, "Declaring the least squares problem.\n");
-    fflush(stdout);
+    if (wRank == 0) {
+        fprintf(stdout, "Declaring the least squares problem.\n");
+        fflush(stdout);
+    }
     alglib::linlsqrcreate(Npts, Npts, state);
     alglib::linlsqrsetcond(state, rel_tol, rel_tol, max_iters);
 
@@ -94,30 +127,77 @@ void Apply_Toroidal_Projection(
     for (int Itime = 0; Itime < Ntime; ++Itime) {
         for (int Idepth = 0; Idepth < Ndepth; ++Idepth) {
 
+            // If single_seed == false, then we were provided seed values
+            if (not(single_seed)) {
+                for (Ilat = 0; Ilat < Nlat; ++Ilat) {
+                    for (Ilon = 0; Ilon < Nlon; ++Ilon) {
+                        index = Index(Itime, Idepth, Ilat, Ilon,
+                                      Ntime, Ndepth, Nlat, Nlon);
+                        index_sub = Index(0, 0, Ilat, Ilon,
+                                          1, 1, Nlat, Nlon);
+                        F_seed.at(index_sub) = seed.at(index);
+                    }
+                }
+            }
+
             //
             //// Build the RHS of the problem (curl term)
             //
-            fprintf(stdout, "Building the RHS of the least squares problem.\n");
-            fflush(stdout);
+            if ( (wRank == 0) and (Itime == 0) ) {
+                fprintf(stdout, "Building the RHS of the least squares problem.\n");
+                fflush(stdout);
+            }
+
+            // Get Lap of computed F term
+            //
+            // The 'seed' approach is a bit different than usual.
+            //   There doesn't seem to be a way to provide an actual
+            //   seed to the solver, so instead I'm going to modify
+            //   the RHS of the problem. That is, if we're solving
+            //   Ax = b, then we write x = x' + x0, where x0 is the
+            //   seed / initial guess. We then instead solve the 
+            //   problem Ax' = b - Ax0
+            //
+            toroidal_Lap_F(F_seed_Lap, F_seed, longitude, latitude,
+                    Ntime, Ndepth, Nlat, Nlon, unmask);
 
             toroidal_curl_u_dot_er(curl_term, u_lon, u_lat, longitude, latitude, 
-                    Itime, Idepth, Ntime, Ndepth, Nlat, Nlon, unmask);
+                    Itime, Idepth, Ntime, Ndepth, Nlat, Nlon, unmask, &F_seed_Lap);
 
             //
             //// Now apply the least-squares solver
             //
-            fprintf(stdout, "Solving the least squares problem.\n");
-            fflush(stdout);
+            if ( (wRank == 0) and (Itime == 0) ) {
+                fprintf(stdout, "Solving the least squares problem.\n");
+                fflush(stdout);
+            }
             alglib::linlsqrsolvesparse(state, Lap, rhs);
             alglib::linlsqrresults(state, F_alglib, report);
 
+            // Extract the solution and add the seed back in
             F_array = F_alglib.getcontent();
             std::vector<double> F_vector(F_array, F_array + Npts);
+            for (size_t ii = 0; ii < F_vector.size(); ++ii) {
+                F_vector.at(ii) += F_seed.at(ii);
+            }
 
-            // Get associated velocity
-            std::vector<double> u_lon_tor(Npts, 0.), u_lat_tor(Npts, 0.);
+            // Get velocity associated to computed F field
+            std::vector<double> 
+                u_lon_tor(Npts, 0.), u_lat_tor(Npts, 0.), div_tor(Npts, 0.);
             toroidal_vel_from_F(u_lon_tor, u_lat_tor, F_vector, longitude, latitude,
                                 Ntime, Ndepth, Nlat, Nlon, unmask);
+            toroidal_vel_div(div_tor, u_lon_tor, u_lat_tor, longitude, latitude,
+                                Ntime, Ndepth, Nlat, Nlon, unmask);
+
+            // Get velocity associated with seed F field
+            std::vector<double> 
+                u_lon_seed(Npts, 0.), u_lat_seed(Npts, 0.);
+            toroidal_vel_from_F(u_lon_seed, u_lat_seed, F_seed, longitude, latitude,
+                                Ntime, Ndepth, Nlat, Nlon, unmask);
+
+            // Get Lap of computed F term
+            toroidal_Lap_F(Lap_F_tor, F_vector, longitude, latitude,
+                    Ntime, Ndepth, Nlat, Nlon, unmask);
 
             //
             //// Store into the full arrays
@@ -130,15 +210,33 @@ void Apply_Toroidal_Projection(
                     index_sub = Index(0, 0, Ilat, Ilon,
                                       1, 1, Nlat, Nlon);
 
-                    full_F.at(index) = F_vector.at(index_sub);
-
                     full_u_lon_tor.at(index) = u_lon_tor.at(index_sub);
                     full_u_lat_tor.at(index) = u_lat_tor.at(index_sub);
+                    full_div_tor.at(  index) = div_tor.at(  index_sub);
+                    full_F.at(        index) = F_vector.at( index_sub);
 
                     full_curl_term.at(index) = curl_term.at(index_sub);
+                    full_Lap_F.at(    index) = Lap_F_tor.at(index_sub);
+
+                    full_u_lon_seed.at(index) = u_lon_seed.at(index_sub);
+                    full_u_lat_seed.at(index) = u_lat_seed.at(index_sub);
+                    full_seed.at(      index) = F_seed.at(    index_sub);
 
                 }
             }
+
+            // Copy this solution into the seed for the next iteration
+            //  if single_seed == true (i.e. carry the seed onwards)
+            if (single_seed) {
+                F_seed = F_vector;
+            }
+
+            // 
+            #if DEBUG >= 0
+            fprintf(stdout, "    Rank %d done time %d\n", wRank, Itime + myStarts.at(0) );
+            fflush(stdout);
+            #endif
+
         }
     }
 
@@ -155,10 +253,19 @@ void Apply_Toroidal_Projection(
         size_t(Nlat), size_t(Nlon)};
 
     std::vector<std::string> vars_to_write;
-    vars_to_write.push_back("F");
     vars_to_write.push_back("u_lon");
     vars_to_write.push_back("u_lat");
+    vars_to_write.push_back("F");
+
+    vars_to_write.push_back("Lap_F");
     vars_to_write.push_back("curl_term");
+
+    vars_to_write.push_back("F_seed");
+    //vars_to_write.push_back("u_lon_seed");
+    //vars_to_write.push_back("u_lat_seed");
+
+    vars_to_write.push_back("div_tor");
+    vars_to_write.push_back("div_orig");
 
     char fname [50];
     snprintf(fname, 50, "toroidal_projection.nc");
@@ -166,10 +273,19 @@ void Apply_Toroidal_Projection(
     initialize_output_file(time, depth, longitude, latitude,
             mask, vars_to_write, fname, 0);
     
-    write_field_to_output(full_u_lon_tor, "u_lon",     starts, counts, fname, &mask);
-    write_field_to_output(full_u_lat_tor, "u_lat",     starts, counts, fname, &mask);
-    write_field_to_output(full_F,         "F",         starts, counts, fname, &mask);
-    write_field_to_output(full_curl_term, "curl_term", starts, counts, fname, &mask);
+    write_field_to_output(full_u_lon_tor,  "u_lon",         starts, counts, fname, &mask);
+    write_field_to_output(full_u_lat_tor,  "u_lat",         starts, counts, fname, &mask);
+    write_field_to_output(full_F,          "F",             starts, counts, fname, &unmask);
+
+    write_field_to_output(full_Lap_F,      "Lap_F",         starts, counts, fname, &mask);
+    write_field_to_output(full_curl_term,  "curl_term",     starts, counts, fname, &mask);
+
+    write_field_to_output(full_seed,       "F_seed",        starts, counts, fname, &mask);
+    //write_field_to_output(full_u_lon_seed, "u_lon_seed",    starts, counts, fname, &mask);
+    //write_field_to_output(full_u_lat_seed, "u_lat_seed",    starts, counts, fname, &mask);
+
+    write_field_to_output(full_div_tor,    "div_tor",       starts, counts, fname, &mask);
+    write_field_to_output(full_div_orig,   "div_orig",      starts, counts, fname, &mask);
 
     add_attr_to_file("rel_tol", rel_tol, fname);
     add_attr_to_file("max_iters", (double) max_iters, fname);
