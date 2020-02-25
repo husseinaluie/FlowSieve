e88abd239eea6ed5bd1a01f50b923c53be45b483
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2020-02-25 13:00:18 -0500

    Updating toroidal projection code, adding potential projection

--- Preprocess/Apply_Potential_Projection.cpp
+++ Preprocess/Apply_Potential_Projection.cpp
@@ -12,7 +12,7 @@
 #include "../ALGLIB/solvers.h"
 
 
-void Apply_Toroidal_Projection(
+void Apply_Potential_Projection(
         std::vector<double> & u_lon,
         std::vector<double> & u_lat,
         const std::vector<double> & time,
@@ -34,7 +34,7 @@ void Apply_Toroidal_Projection(
 
     const double rel_tol    = 1e-7;
     const int    max_iters  = 1e4;
-    const bool   weight_err = true; // weight err by cell size
+    const bool   weight_err = true;
 
     // Create a 'no mask' mask variable
     //   we'll treat land values as zero velocity
@@ -81,20 +81,19 @@ void Apply_Toroidal_Projection(
     // Storage vectors
     std::vector<double> 
         full_F(         u_lon.size(), 0. ),
-        full_u_lon_tor( u_lon.size(), 0. ),
-        full_u_lat_tor( u_lon.size(), 0. ),
-        full_div_tor(   u_lon.size(), 0. ),
+        full_u_lon_pot( u_lon.size(), 0. ),
+        full_u_lat_pot( u_lon.size(), 0. ),
         full_RHS(       u_lon.size(), 0. ),
+        full_div_pot(   u_lon.size(), 0. ),
         full_seed(      u_lon.size(), 0. );
 
     // alglib variables
     alglib::real_1d_array rhs;
     std::vector<double> 
-        curl_term( Npts, 0.), 
-        Lap_F_tor( Npts, 0.),
-        F_seed(    Npts, 0.),
-        F_seed_Lap(Npts, 0.);
-    
+        div_term(   Npts, 0. ), 
+        Lap_F_pot(  Npts, 0. ),
+        F_seed(     Npts, 0. ),
+        F_seed_Lap( Npts, 0. );
 
     // Copy the starting seed.
     if (single_seed) {
@@ -107,7 +106,7 @@ void Apply_Toroidal_Projection(
         }
     }
 
-    rhs.attach_to_ptr(Npts, &curl_term[0]);
+    rhs.attach_to_ptr(Npts, &div_term[0]);
 
     alglib::linlsqrstate state;
     alglib::linlsqrreport report;
@@ -175,18 +174,16 @@ void Apply_Toroidal_Projection(
             toroidal_Lap_F(F_seed_Lap, F_seed, longitude, latitude,
                     Ntime, Ndepth, Nlat, Nlon, unmask);
 
-            toroidal_curl_u_dot_er(curl_term, u_lon, u_lat, longitude, latitude, 
-                    Itime, Idepth, Ntime, Ndepth, Nlat, Nlon, unmask, &F_seed_Lap);
+            for (Ilat = 0; Ilat < Nlat; ++Ilat) {
+                for (Ilon = 0; Ilon < Nlon; ++Ilon) {
+                    index = Index(Itime, Idepth, Ilat, Ilon,
+                                  Ntime, Ndepth, Nlat, Nlon);
+                    index_sub = Index(0, 0, Ilat, Ilon,
+                                      1, 1, Nlat, Nlon);
+                    div_term.at(index_sub) = 
+                        full_div_orig.at(index) - F_seed_Lap.at(index_sub);
 
-            if (weight_err) {
-                // Weight by area if requested
-                for (Ilat = 0; Ilat < Nlat; ++Ilat) {
-                    for (Ilon = 0; Ilon < Nlon; ++Ilon) {
-                        index_sub = Index(0, 0, Ilat, Ilon,
-                                          1, 1, Nlat, Nlon);
-
-                        curl_term.at(index_sub) *= dAreas.at(index_sub);
-                    }
+                    if (weight_err) { div_term.at(index_sub) *= dAreas.at(index_sub); }
                 }
             }
 
@@ -209,21 +206,11 @@ void Apply_Toroidal_Projection(
 
             // Get velocity associated to computed F field
             std::vector<double> 
-                u_lon_tor(Npts, 0.), u_lat_tor(Npts, 0.), div_tor(Npts, 0.);
-            toroidal_vel_from_F(u_lon_tor, u_lat_tor, F_vector, longitude, latitude,
+                u_lon_pot(Npts, 0.), u_lat_pot(Npts, 0.), div_pot(Npts, 0.);
+            potential_vel_from_F(u_lon_pot, u_lat_pot, F_vector, longitude, latitude,
                                 Ntime, Ndepth, Nlat, Nlon, unmask);
-            toroidal_vel_div(div_tor, u_lon_tor, u_lat_tor, longitude, latitude,
+            toroidal_vel_div(div_pot, u_lon_pot, u_lat_pot, longitude, latitude,
                                 Ntime, Ndepth, Nlat, Nlon, unmask);
-
-            // Get velocity associated with seed F field
-            std::vector<double> 
-                u_lon_seed(Npts, 0.), u_lat_seed(Npts, 0.);
-            toroidal_vel_from_F(u_lon_seed, u_lat_seed, F_seed, longitude, latitude,
-                                Ntime, Ndepth, Nlat, Nlon, unmask);
-
-            // Get Lap of computed F term
-            toroidal_Lap_F(Lap_F_tor, F_vector, longitude, latitude,
-                    Ntime, Ndepth, Nlat, Nlon, unmask);
 
             //
             //// Store into the full arrays
@@ -239,16 +226,14 @@ void Apply_Toroidal_Projection(
                                       1, 1, Ntime, Ndepth);
 
                     // add the mean velocity back in
-                    full_u_lon_tor.at(index) = u_lon_tor.at(index_sub) 
-                        + u_lon_means.at(mean_ind);
-                    full_u_lat_tor.at(index) = u_lat_tor.at(index_sub)
-                        + u_lat_means.at(mean_ind);
+                    full_u_lon_pot.at(index) = u_lon_pot.at(index_sub);
+                    full_u_lat_pot.at(index) = u_lat_pot.at(index_sub);
 
-                    full_div_tor.at(  index) = div_tor.at(  index_sub);
+                    full_div_pot.at(  index) = div_pot.at(  index_sub);
                     full_F.at(        index) = F_vector.at( index_sub);
 
                     full_seed.at(index) = F_seed.at(   index_sub);
-                    full_RHS.at( index) = curl_term.at(index_sub);
+                    full_RHS.at( index) = div_term.at( index_sub);
 
                 }
             }
@@ -288,24 +273,24 @@ void Apply_Toroidal_Projection(
 
     vars_to_write.push_back("RHS");
 
-    vars_to_write.push_back("div_tor");
+    vars_to_write.push_back("div_pot");
     vars_to_write.push_back("div_orig");
 
     char fname [50];
-    snprintf(fname, 50, "toroidal_projection.nc");
+    snprintf(fname, 50, "potential_projection.nc");
 
     initialize_output_file(time, depth, longitude, latitude,
             mask, vars_to_write, fname, 0);
     
-    write_field_to_output(full_u_lon_tor,  "u_lon",    starts, counts, fname, &mask);
-    write_field_to_output(full_u_lat_tor,  "u_lat",    starts, counts, fname, &mask);
+    write_field_to_output(full_u_lon_pot,  "u_lon",    starts, counts, fname, &mask);
+    write_field_to_output(full_u_lat_pot,  "u_lat",    starts, counts, fname, &mask);
 
     write_field_to_output(full_RHS,        "RHS",      starts, counts, fname, &mask);
 
     write_field_to_output(full_F,          "F",        starts, counts, fname, &unmask);
     write_field_to_output(full_seed,       "F_seed",   starts, counts, fname, &mask);
 
-    write_field_to_output(full_div_tor,    "div_tor",  starts, counts, fname, &mask);
+    write_field_to_output(full_div_pot,    "div_pot",  starts, counts, fname, &mask);
     write_field_to_output(full_div_orig,   "div_orig", starts, counts, fname, &mask);
 
     add_attr_to_file("rel_tol", rel_tol, fname);
