87ef2b2e69af7419ff07910a57c9165bfaad8949
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2020-03-03 14:36:33 -0500

    Minor reformat

--- Functions/KE_from_vels.cpp
+++ Functions/KE_from_vels.cpp
@@ -12,17 +12,21 @@ void KE_from_vels(
         ) {
 
     size_t index;
+    double tmp;
 
-    #pragma omp parallel default(none) private(index) shared(KE, u1, u2, u3, mask)
+    #pragma omp parallel default(none) private(index, tmp) shared(KE, u1, u2, u3, mask)
     {
         #pragma omp for collapse(1) schedule(guided)
         for (index = 0; index < u1->size(); ++index) {
 
             if (mask.at(index) == 1) { // Skip land areas
-                KE.at(index) = 0.;
-                if (u1 != NULL) { KE.at(index) += 0.5 * rho0 * pow(u1->at(index), 2); }
-                if (u2 != NULL) { KE.at(index) += 0.5 * rho0 * pow(u2->at(index), 2); }
-                if (u3 != NULL) { KE.at(index) += 0.5 * rho0 * pow(u3->at(index), 2); }
+                tmp = 0.;
+                if (u1 != NULL) { tmp += 0.5 * rho0 * pow(u1->at(index), 2); }
+                if (u2 != NULL) { tmp += 0.5 * rho0 * pow(u2->at(index), 2); }
+                if (u3 != NULL) { tmp += 0.5 * rho0 * pow(u3->at(index), 2); }
+                KE.at(index) = tmp;
+            } else {
+                KE.at(index) = constants::fill_value;
             }
         }
     }
