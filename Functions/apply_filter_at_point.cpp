2ace90f3ce9e3471824f502518cbade449be19cf
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2020-06-15 17:09:29 -0400

    Adding option for weight in filtering.
    This makes it easy to compute "tilde" filters by simply
    passing rho in as the weight

--- Functions/apply_filter_at_point.cpp
+++ Functions/apply_filter_at_point.cpp
@@ -24,13 +24,14 @@ void apply_filter_at_point(
         const double scale,
         const std::vector<double> & mask,
         const std::vector<bool> & use_mask,
-        const std::vector<double> * local_kernel
+        const std::vector<double> * local_kernel,
+        const std::vector<double> * weight
         ) {
 
     assert(coarse_vals.size() == fields.size());
     const size_t Nfields = fields.size();
 
-    double dist, kern, area;
+    double dist, kern, area, loc_val;
     int index, area_index;
     int curr_lon, curr_lat;
 
@@ -96,7 +97,9 @@ void apply_filter_at_point(
 
             for (size_t II = 0; II < Nfields; ++II) {
                 mask_val = use_mask.at(II) ? mask.at(index) : 1.;
-                tmp_vals.at(II) += fields.at(II)->at(index) * kern * area * mask_val;
+                loc_val = fields.at(II)->at(index);
+                if (weight != NULL) { loc_val *= weight->at(index); }
+                tmp_vals.at(II) += loc_val * kern * area * mask_val;
             }
 
         }
@@ -104,6 +107,8 @@ void apply_filter_at_point(
 
     // On the off chance that the kernel was null (size zero), just return zero
     for (size_t II = 0; II < Nfields; ++II) {
-        *(coarse_vals.at(II)) = (kA_sum == 0) ? 0. : tmp_vals.at(II) / kA_sum;
+        if (coarse_vals.at(II) != NULL) {
+            *(coarse_vals.at(II)) = (kA_sum == 0) ? 0. : tmp_vals.at(II) / kA_sum;
+        }
     }
 }
