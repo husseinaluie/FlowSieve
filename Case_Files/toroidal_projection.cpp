7b5595c8358f02c6972d78af8a7c5230ab45a375
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2020-07-16 12:33:35 -0400

    Updating projection casefiles with newer format

--- Case_Files/toroidal_projection.cpp
+++ Case_Files/toroidal_projection.cpp
@@ -37,34 +37,34 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank( MPI_COMM_WORLD, &wRank );
     MPI_Comm_size( MPI_COMM_WORLD, &wSize );
 
-    #if DEBUG >= 0
-    if (wRank == 0) {
-        if (constants::CARTESIAN) { 
-            fprintf(stdout, "Using Cartesian coordinates.\n");
-        } else {
-            fprintf(stdout, "Using spherical coordinates.\n");
-        }
-        fflush(stdout);
+    //
+    //// Parse command-line arguments
+    //
+    InputParser input(argc, argv);
+    if(input.cmdOptionExists("--version")){
+        if (wRank == 0) { print_compile_info(NULL); } 
+        return 0;
     }
-    #endif
-    MPI_Barrier(MPI_COMM_WORLD);
+
+    // first argument is the flag, second argument is default value (for when flag is not present)
+    const std::string &input_fname  = input.getCmdOption("--input_file",   "input.nc");
+    const std::string &output_fname = input.getCmdOption("--output_file",  "toroidal_projection.nc");
+    const std::string &seed_fname   = input.getCmdOption("--seed_file",    "seed.nc");
+
+    const std::string &time_dim_name      = input.getCmdOption("--time",        "time");
+    const std::string &depth_dim_name     = input.getCmdOption("--depth",       "depth");
+    const std::string &latitude_dim_name  = input.getCmdOption("--latitude",    "latitude");
+    const std::string &longitude_dim_name = input.getCmdOption("--longitude",   "longitude");
+
+    const std::string &zonal_vel_name    = input.getCmdOption("--zonal_vel",   "uo");
+    const std::string &merid_vel_name    = input.getCmdOption("--merid_vel",   "vo");
 
     // Print processor assignments
     const int max_threads = omp_get_max_threads();
     omp_set_num_threads( max_threads );
-    #if DEBUG >= 2
-    int tid, nthreads;
-    #pragma omp parallel default(none) private(tid, nthreads) \
-        shared(stdout) firstprivate(wRank, wSize)
-    {
-        tid = omp_get_thread_num();
-        nthreads = omp_get_num_threads();
-        fprintf(stdout, "Hello from thread %d of %d on processor %d of %d.\n", 
-                tid+1, nthreads, wRank+1, wSize);
-    }
-    fflush(stdout);
-    MPI_Barrier(MPI_COMM_WORLD);
-    #endif
+
+    // Print some header info, depending on debug level
+    print_header_info();
 
     std::vector<double> longitude, latitude, time, depth;
     std::vector<double> u_lon, u_lat, seed;
@@ -77,59 +77,26 @@ int main(int argc, char *argv[]) {
     #endif
 
     // Read in the grid coordinates
-    read_var_from_file(longitude, "longitude", "input.nc");
-    read_var_from_file(latitude,  "latitude",  "input.nc");
-    read_var_from_file(time,      "time",      "input.nc");
-    read_var_from_file(depth,     "depth",     "input.nc");
+    read_var_from_file(longitude, longitude_dim_name, input_fname);
+    read_var_from_file(latitude,  latitude_dim_name,  input_fname);
+    read_var_from_file(time,      time_dim_name,      input_fname);
+    read_var_from_file(depth,     depth_dim_name,     input_fname);
+     
+    convert_coordinates(longitude, latitude);
 
     // Read in the velocity fields
-    read_var_from_file(u_lon, "uo",  "input.nc", &mask, &myCounts, &myStarts);
-    read_var_from_file(u_lat, "vo",  "input.nc", &mask, &myCounts, &myStarts);
+    read_var_from_file(u_lon, zonal_vel_name,  input_fname, &mask, &myCounts, &myStarts);
+    read_var_from_file(u_lat, merid_vel_name,  input_fname, &mask, &myCounts, &myStarts);
 
     // Read in the seed
     double seed_count;
-    read_attr_from_file(seed_count, "seed_count", "seed.nc");
+    read_attr_from_file(seed_count, "seed_count", seed_fname);
     const bool single_seed = (seed_count < 2);
-    if (wRank == 0) { 
-        fprintf(stdout, " single_seed = %s\n", single_seed ? "true" : "false"); 
-    }
-    //read_var_from_file(seed, "seed",  "seed.nc", NULL, NULL, NULL, false);
-    read_var_from_file(seed, "seed",  "seed.nc", NULL, NULL, NULL, not(single_seed));
+    if (wRank == 0) { fprintf(stdout, " single_seed = %s\n", single_seed ? "true" : "false"); }
+    read_var_from_file(seed, "seed", seed_fname, NULL, NULL, NULL, not(single_seed));
 
-    #if DEBUG >= 1
-    const int Ntime  = time.size();
-    const int Ndepth = depth.size();
-    #endif
     const int Nlon   = longitude.size();
     const int Nlat   = latitude.size();
-
-    #if DEBUG >= 1
-    fprintf(stdout, "Processor %d has (%d, %d, %d, %d) from (%d, %d, %d, %d)\n", 
-            wRank, 
-            myCounts[0], myCounts[1], myCounts[2], myCounts[3],
-            Ntime, Ndepth, Nlat, Nlon);
-    fflush(stdout);
-    MPI_Barrier(MPI_COMM_WORLD);
-    #endif
-
-    if (not(constants::CARTESIAN)) {
-        // Convert coordinate to radians
-        if (wRank == 0) { fprintf(stdout, "Converting to radians.\n\n"); }
-        int ii;
-        const double D2R = M_PI / 180.;
-        #pragma omp parallel default(none) private(ii) shared(longitude, latitude)
-        { 
-            #pragma omp for collapse(1) schedule(static)
-            for (ii = 0; ii < Nlon; ii++) {
-                longitude.at(ii) = longitude.at(ii) * D2R;
-            }
-
-            #pragma omp for collapse(1) schedule(static)
-            for (ii = 0; ii < Nlat; ii++) {
-                latitude.at(ii) = latitude.at(ii) * D2R;
-            }
-        }
-    }
 
     // Compute the area of each 'cell'
     //   which will be necessary for integration
@@ -141,6 +108,7 @@ int main(int argc, char *argv[]) {
     compute_areas(areas, longitude, latitude);
 
     Apply_Toroidal_Projection(
+            output_fname,
             u_lon, u_lat, time, depth, latitude, longitude,
             areas, mask, myCounts, myStarts, seed, single_seed
             );
