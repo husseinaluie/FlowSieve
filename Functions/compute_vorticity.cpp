acbefa1230534cfe45878f24ae0174d0c71830a7
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2021-02-08 11:35:02 -0500

    Merging divergence and Okubo-Weiss into vorticity calculator This avoids repeated derivative calls

--- Functions/compute_vorticity.cpp
+++ Functions/compute_vorticity.cpp
@@ -7,6 +7,8 @@ void compute_vorticity(
         std::vector<double> & vort_r,           /**< [in] where to store vort_r */
         std::vector<double> & vort_lon,         /**< [in] where to store vort_lon */
         std::vector<double> & vort_lat,         /**< [in] where to store vort_lat */
+        std::vector<double> & vel_div,
+        std::vector<double> & OkuboWeiss,
         const std::vector<double> & u_r,        /**< [in] full u_r for calculation */
         const std::vector<double> & u_lon,      /**< [in] full u_lon for calculation */
         const std::vector<double> & u_lat,      /**< [in] full u_lat for calculation */
@@ -26,10 +28,19 @@ void compute_vorticity(
     MPI_Comm_rank( comm, &wRank );
     MPI_Comm_size( comm, &wSize );
 
-    double vort_r_tmp, vort_lon_tmp, vort_lat_tmp;
+    double vort_r_tmp, vort_lon_tmp, vort_lat_tmp, div_tmp, OkuboWeiss_tmp;
     int Itime, Idepth, Ilat, Ilon;
     size_t index; 
     const size_t Npts = u_r.size();
+
+    // If any of the 'output' arrays are size zero, don't do them (this is essentially how to 'turn off' outputs)
+    const bool do_vort_r   = vort_r.size() > 0;
+    const bool do_vort_lon = vort_lon.size() > 0;
+    const bool do_vort_lat = vort_lat.size() > 0;
+
+    const bool do_vel_div = vel_div.size() > 0;
+
+    const bool do_OkuboWeiss = OkuboWeiss.size() > 0;
 
     #if DEBUG >= 2
     if (wRank == 0) { fprintf(stdout, "  Starting vorticity computation.\n"); }
@@ -38,9 +49,11 @@ void compute_vorticity(
     #pragma omp parallel \
     default(none) \
     shared(mask, u_r, u_lon, u_lat, longitude, latitude,\
-            vort_r, vort_lon, vort_lat)\
+            vort_r, vort_lon, vort_lat,\
+            vel_div, OkuboWeiss)\
     private(Itime, Idepth, Ilat, Ilon, index, \
-            vort_r_tmp, vort_lon_tmp, vort_lat_tmp)
+            vort_r_tmp, vort_lon_tmp, vort_lat_tmp,\
+            div_tmp, OkuboWeiss_tmp)
     {
         #pragma omp for collapse(1) schedule(guided, OMP_chunksize)
         for (index = 0; index < Npts; index++) {
@@ -49,6 +62,10 @@ void compute_vorticity(
             vort_lon_tmp = constants::fill_value;
             vort_lat_tmp = constants::fill_value;
 
+            div_tmp = constants::fill_value;
+
+            OkuboWeiss_tmp = constants::fill_value;
+
             if ( mask.at(index) ) { // Skip land areas
 
                 Index1to4(index, Itime, Idepth, Ilat, Ilon,
@@ -56,6 +73,8 @@ void compute_vorticity(
 
                 compute_vorticity_at_point(
                         vort_r_tmp, vort_lon_tmp, vort_lat_tmp,
+                        div_tmp, 
+                        OkuboWeiss_tmp,
                         u_r,        u_lon,        u_lat,
                         Ntime, Ndepth, Nlat, Nlon,
                         Itime, Idepth, Ilat, Ilon,
@@ -63,9 +82,13 @@ void compute_vorticity(
                         mask);
             }
 
-            vort_r.at(  index) = vort_r_tmp;
-            vort_lon.at(index) = vort_lon_tmp;
-            vort_lat.at(index) = vort_lat_tmp;
+            if (do_vort_r)   { vort_r.at(  index) = vort_r_tmp; }
+            if (do_vort_lon) { vort_lon.at(index) = vort_lon_tmp; }
+            if (do_vort_lat) { vort_lat.at(index) = vort_lat_tmp; }
+
+            if (do_vel_div) { vel_div.at(index) = div_tmp; }
+
+            if (do_OkuboWeiss) { OkuboWeiss.at(index) = OkuboWeiss_tmp; }
 
         } // end index loop
     } // end pragma
--- Functions/compute_vorticity_at_point.cpp
+++ Functions/compute_vorticity_at_point.cpp
@@ -9,6 +9,8 @@ void compute_vorticity_at_point(
         double & vort_r_tmp,                    /**< [in] where to store vort_r */
         double & vort_lon_tmp,                  /**< [in] where to store vort_lon */
         double & vort_lat_tmp,                  /**< [in] where to store vort_lat */
+        double & div_tmp,
+        double & OkuboWeiss_tmp,
         const std::vector<double> & u_r,        /**< [in] full u_r for calculation */
         const std::vector<double> & u_lon,      /**< [in] full u_lon for calculation */
         const std::vector<double> & u_lat,      /**< [in] full u_lat for calculation */
@@ -34,10 +36,12 @@ void compute_vorticity_at_point(
 
     if (constants::CARTESIAN) {
 
-        double ux_y, ux_z, uy_x, uy_z, uz_x, uz_y;
-        std::vector<double*> x_deriv_vals {NULL,  &uy_x, &uz_x};
-        std::vector<double*> y_deriv_vals {&ux_y, NULL,  &uz_y};
-        std::vector<double*> z_deriv_vals {&ux_z, &uy_z, NULL };
+        double ux_x, ux_y, ux_z,
+               uy_x, uy_y, uy_z,
+               uz_x, uz_y, uz_z;
+        std::vector<double*> x_deriv_vals {&ux_x, &uy_x, &uz_x};
+        std::vector<double*> y_deriv_vals {&ux_y, &uy_y, &uz_y};
+        std::vector<double*> z_deriv_vals {&ux_z, &uy_z, &uz_z};
 
         Cart_derivatives_at_point(
            x_deriv_vals, y_deriv_vals,
@@ -50,16 +54,22 @@ void compute_vorticity_at_point(
         vort_lon_tmp = uz_y - uy_z;
         vort_lat_tmp = ux_z - uz_x;
         vort_r_tmp   = uy_x - ux_y;
+
+        div_tmp = ux_x + uy_y + uz_z;
+
+        OkuboWeiss_tmp = pow(ux_x - uy_y, 2) + 4 * ux_y * uy_x;
     } else {
 
-        int index = Index(Itime, Idepth, Ilat, Ilon,
-                          Ntime, Ndepth, Nlat, Nlon);
-        double ur_lon, ur_lat, ulon_r, ulon_lat, ulat_r, ulat_lon;
+        size_t index = Index(Itime, Idepth, Ilat, Ilon,
+                             Ntime, Ndepth, Nlat, Nlon);
+        double ur_r,   ur_lon,   ur_lat, 
+               ulon_r, ulon_lon, ulon_lat, 
+               ulat_r, ulat_lon, ulat_lat;
         double lat = latitude.at(Ilat);
 
-        std::vector<double*> lon_deriv_vals {NULL,      &ulat_lon, &ur_lon};
-        std::vector<double*> lat_deriv_vals {&ulon_lat, NULL,      &ur_lat};
-        std::vector<double*> r_deriv_vals   {&ulon_r,   &ulat_r,   NULL   };
+        std::vector<double*> lon_deriv_vals {&ulon_lon, &ulat_lon, &ur_lon};
+        std::vector<double*> lat_deriv_vals {&ulon_lat, &ulat_lat, &ur_lat};
+        std::vector<double*> r_deriv_vals   {&ulon_r,   &ulat_r,   &ur_r  };
 
         spher_derivative_at_point(
                 lat_deriv_vals, deriv_fields,
@@ -75,15 +85,32 @@ void compute_vorticity_at_point(
                 Ntime, Ndepth, Nlat, Nlon,
                 mask);
 
-        // Longitudinal derivative component
-        vort_r_tmp += constants::R_earth * ulat_lon;
+        const double cos_lat = cos(lat);
+        const double sin_lat = sin(lat);
+        const double tan_lat = tan(lat);
 
-        // Latitudinal derivative component
-        //  - ddlat ( u_lon * cos(lat) ) = u_lon * sin(lat) - ddlat( u_lon ) * cos(lat)
-        vort_r_tmp += constants::R_earth
-            * (sin(lat) * u_lon.at(index)  -  cos(lat) * ulon_lat);
+        //
+        //// First, do vorticity
+        //
+        vort_r_tmp   = ( ulat_lon - cos_lat * ulon_lat + sin_lat * u_lon.at(index) ) / ( constants::R_earth * cos_lat );
+        vort_lon_tmp = ( ur_lat - constants::R_earth * ulat_r - u_lat.at(index) ) / ( constants::R_earth );
+        vort_lat_tmp = ( cos_lat * u_lon.at(index) + constants::R_earth * cos_lat * ulon_r - ur_lon ) / ( constants::R_earth * cos_lat );
 
-        // Scale
-        vort_r_tmp *= 1. / ( pow(constants::R_earth, 2) * cos(lat) );
+        //
+        //// Now the divergence
+        //
+        div_tmp =   ( 2 * u_r.at(index) / constants::R_earth )
+                  + ( ur_r )
+                  + ( ulon_lon / ( constants::R_earth * cos_lat ) )
+                  + ( ulat_lat / constants::R_earth )
+                  - ( u_lat.at(index) * tan_lat / constants::R_earth );
+
+        //
+        //// Now the Okubo-Weiss parameter
+        //
+        const double s_n = ( cos_lat * ulon_lon - ulat_lat ) / constants::R_earth;
+        const double s_s = ( cos_lat * ulat_lon + ulon_lat ) / constants::R_earth;
+        OkuboWeiss_tmp = pow(s_n, 2) + pow(s_s, 2) - pow(vort_r_tmp, 2);
+
     }
 }
