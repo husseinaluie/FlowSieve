c67e02affcfa1546c6205328ea81643ac10a75e5
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2021-02-08 11:32:31 -0500

    Saving changes before deleting file (as a precaution)

--- Functions/compute_div_vel.cpp
+++ Functions/compute_div_vel.cpp
@@ -8,6 +8,7 @@
 
 void compute_div_vel(
         std::vector<double> & div,
+        std::vector<double> & OkuboWeiss,
         const std::vector<double> & u_x,
         const std::vector<double> & u_y,
         const std::vector<double> & u_z,
@@ -22,13 +23,15 @@ void compute_div_vel(
 
     const int OMP_chunksize = get_omp_chunksize(Nlat,Nlon);
 
-    double div_tmp;
+    double div_tmp, OkuboWeiss_tmp;
 
     int Itime, Idepth, Ilat, Ilon;
     size_t index;
     const size_t Npts = u_x.size();
 
-    double ux_x, uy_y, uz_z;
+    double ux_x, ux_y, ux_z,
+           uy_x, uy_y, uy_z,
+           uz_x, uz_y, uz_z;
     std::vector<double*> x_deriv_vals, y_deriv_vals, z_deriv_vals;
     std::vector<const std::vector<double>*> deriv_fields;
 
@@ -38,29 +41,31 @@ void compute_div_vel(
     
     #pragma omp parallel \
     default(none) \
-    shared( div, latitude, longitude, mask,\
+    shared( div, OkuboWeiss, latitude, longitude, mask,\
             u_x, u_y, u_z, deriv_fields)\
     private(Itime, Idepth, Ilat, Ilon, index,\
-            ux_x, uy_y, uz_z, x_deriv_vals, y_deriv_vals, z_deriv_vals,\
-            div_tmp)
+            ux_x, ux_y, ux_z, uy_x, uy_y, uy_z, uz_x, uz_y, uz_z, \
+            x_deriv_vals, y_deriv_vals, z_deriv_vals,\
+            div_tmp, OkuboWeiss_tmp)
     {
 
         x_deriv_vals.push_back(&ux_x);
-        x_deriv_vals.push_back(NULL);
-        x_deriv_vals.push_back(NULL);
+        x_deriv_vals.push_back(&uy_x);
+        x_deriv_vals.push_back(&uz_x);
 
-        y_deriv_vals.push_back(NULL);
+        y_deriv_vals.push_back(&ux_y);
         y_deriv_vals.push_back(&uy_y);
-        y_deriv_vals.push_back(NULL);
+        y_deriv_vals.push_back(&uz_y);
 
-        z_deriv_vals.push_back(NULL);
-        z_deriv_vals.push_back(NULL);
+        z_deriv_vals.push_back(&ux_z);
+        z_deriv_vals.push_back(&uy_z);
         z_deriv_vals.push_back(&uz_z);
 
         #pragma omp for collapse(1) schedule(guided, OMP_chunksize)
         for (index = 0; index < Npts; index++) {
 
             div_tmp = constants::fill_value;
+            OkuboWeiss_tmp = constants::fill_value;
 
             if ( mask.at(index) ) { // Skip land areas
 
@@ -78,9 +83,14 @@ void compute_div_vel(
                 // u_i,i
                 div_tmp = ux_x + uy_y + uz_z;
 
+                // 2D Okubo-Weiss
+                // (u_1,1 + u_2,2)^2 + 4 u_1,2 u_2,1
+                OkuboWeiss_tmp = pow(ux_x - uy_y, 2) + 4 * ux_y * uy_x;
+
             }
 
             div.at(index) = div_tmp;
+            OkuboWeiss.at(index) = OkuboWeiss_tmp;
 
         } // end index
     } // end pragma
