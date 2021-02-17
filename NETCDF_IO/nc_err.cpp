63669b7c99f96548ca53f551c4a2c43701cdbba8
Benjamin Storer <benjamin.storer@uwaterloo.ca>
2021-02-17 16:03:52 -0500

    Making netcdf errors fatal.
    (repeatedly) accidentally trying to read from a variable that doesn't exist
    finally became enough of a hassle that netcdf errors have been promoted to be fatal
    
    Setting DEBUG <= -2 squashes this.

--- NETCDF_IO/nc_err.cpp
+++ NETCDF_IO/nc_err.cpp
@@ -1,6 +1,7 @@
 #include "../netcdf_io.hpp"
 #include <string.h>
 #include "../constants.hpp"
+#include <cassert>
 
 void NC_ERR(
         const int e,
@@ -9,7 +10,11 @@ void NC_ERR(
         ) {
 
     #if DEBUG <= -2
+    // Print error statement
     fprintf(stderr, "Error: [%s] at line %d in %s\n", nc_strerror(e), line_num, file_name);
+
+    // Die (ungracefully)
+    assert(false);
     #endif
 
 }
