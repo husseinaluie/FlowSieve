5e684e65733d66bb7a0188d1ae8433921667d12c
Benjamin A. Storer <storer.ben@gmail.com>
2021-04-27 10:53:53 -0400

    Removing only (commented out) code. Also, this function is no longer used.

--- Functions/roll_field.cpp
+++ Functions/roll_field.cpp
@@ -11,7 +11,7 @@
  * Currently hard-coded to roll along lon dimension only.
  *
  * @param[in,out]   field_to_roll               field to be rolled
- * @param[in]       dimension                   dimension to roll (currently must be 0)
+ * @param[in]       dimension                   dimension to roll (currently must be "lon")
  * @param[in]       roll_count                  ammount by which to roll (currently must be 1)
  * @param[in]       Ntime,Ndepth,Nlat,Nlon      (MPI-local) dimension sizes
  *
@@ -36,7 +36,6 @@ void roll_field(
     std::vector<double> work_arr( Nlon );
 
     std::vector<double>::reverse_iterator rbegin, rend;
-    //std::reverse_iterator<std::string::iterator>
 
     for (int Itime = 0; Itime < Ntime; Itime++) {
         for (int Idepth = 0; Idepth < Ndepth; Idepth++) {
@@ -50,29 +49,6 @@ void roll_field(
                 rend    = rbegin + Nlon;
 
                 std::rotate( rbegin, rbegin + roll_count, rend );
-
-                /*
-                // First, pull out the Ilon slice and store it in the work array
-                for (int Ilon = 0; Ilon < Nlon; Ilon++) {
-                    index = Index(Itime, Idepth, Ilat, Ilon,
-                                  Ntime, Ndepth, Nlat, Nlon);
-                    work_arr.at(Ilon) = field_to_roll.at(index);
-                }
-
-                // Now rotate the work array
-                if (roll_count > 0) {
-                    std::rotate( work_arr.rbegin(), work_arr.rbegin() + roll_count, work_arr.rend());
-                } else {
-                    std::rotate( work_arr.begin(),  work_arr.begin() - roll_count,  work_arr.end());
-                }
-
-                // Now copy the rotate vector back into the main array
-                for (int Ilon = 0; Ilon < Nlon; Ilon++) {
-                    index = Index(Itime, Idepth, Ilat, Ilon,
-                                  Ntime, Ndepth, Nlat, Nlon);
-                    field_to_roll.at(index) = work_arr.at(Ilon);
-                }
-                */
 
             }
         }
