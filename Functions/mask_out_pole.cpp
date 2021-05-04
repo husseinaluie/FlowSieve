972b100df43d501e51db8051b413878f0f80a528
Benjamin A. Storer <storer.ben@gmail.com>
2021-05-04 09:30:38 -0400

    Tidying up mask_out_pole print messages. Should now only print once per latitude

--- Functions/mask_out_pole.cpp
+++ Functions/mask_out_pole.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <omp.h>
 #include <stdlib.h>
+#include <mpi.h>
 
 #include "../constants.hpp"
 #include "../functions.hpp"
@@ -26,30 +27,41 @@ void mask_out_pole(
         const int Nlon
         ){
 
+    int wRank=-1, wSize=-1;
+    MPI_Comm_rank( MPI_COMM_WORLD, &wRank );
+    MPI_Comm_size( MPI_COMM_WORLD, &wSize );
+
     int Itime, Idepth, Ilat, Ilon;
     size_t index;
     
     const double D2R = M_PI / 180.;
     const double pole_cut = (90. - 0.1) * D2R;
 
+    std::vector<int> masked_latitudes;
+
     if (not(constants::CARTESIAN)) {
         #pragma omp parallel default(none) \
             private(Itime, Idepth, Ilat, Ilon, index) \
-            shared(latitude, mask, stdout)
+            shared(latitude, mask, masked_latitudes)
         { 
             #pragma omp for collapse(1) schedule(static)
             for (index = 0; index < mask.size(); ++index) {
                 Index1to4(index, Itime, Idepth, Ilat, Ilon,
                                  Ntime, Ndepth, Nlat, Nlon);
                 if ( fabs(latitude.at(Ilat)) >= pole_cut) {
-                    #if DEBUG >= 0
                     if ( (Itime == 0) and (Idepth == 0) and (Ilon == 0) ) {
-                        fprintf(stdout, "Masking out Ilat %d (too close to pole (%g))\n", Ilat, latitude.at(Ilat)/D2R);
+                        masked_latitudes.push_back(Ilat);
                     }
-                    #endif
                     mask.at(index) = false;
                 }
             }
         }
     }
+
+    #if DEBUG >= 0
+    for ( size_t ind = 0; ind < masked_latitudes.size(); ++ind ) {
+        Ilat = masked_latitudes.at( ind );
+        if (wRank == 0) { fprintf(stdout, "Masking out Ilat %d (too close to pole (%g))\n", Ilat, latitude.at(Ilat)/D2R); }
+    }
+    #endif
 }
