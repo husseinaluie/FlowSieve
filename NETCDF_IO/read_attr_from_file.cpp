6773f7b6dff464b6817db7182a418c4aa3110066
Benjamin A. Storer <storer.ben@gmail.com>
2021-08-06 17:15:58 -0400

    Making attribute reading a little more type-resistent
    Reading attributes no longer breaks if int, short, long, etc

--- NETCDF_IO/read_attr_from_file.cpp
+++ NETCDF_IO/read_attr_from_file.cpp
@@ -50,7 +50,45 @@ void read_attr_from_file(
     char attrname [50];
     snprintf(attrname, 50, attr_name);
 
-    nc_get_att(ncid, var_id, attrname, &attr);
+    nc_type att_type;
+    nc_inq_atttype(ncid, var_id, attrname, &att_type);
+    #if DEBUG >= 1
+    switch (att_type) {
+        case NC_SHORT   : fprintf( stdout, "Attribute type is NC_SHORT" ); break;
+        case NC_USHORT  : fprintf( stdout, "Attribute type is NC_USHORT" ); break;
+        case NC_INT     : fprintf( stdout, "Attribute type is NC_INT" ); break;
+        case NC_UINT    : fprintf( stdout, "Attribute type is NC_UINT" ); break;
+        case NC_INT64   : fprintf( stdout, "Attribute type is NC_INT64" ); break;
+        case NC_UINT64  : fprintf( stdout, "Attribute type is NC_UINT64" ); break;
+        case NC_FLOAT   : fprintf( stdout, "Attribute type is NC_FLOAT" ); break;
+        case NC_DOUBLE  : fprintf( stdout, "Attribute type is NC_DOUBLE" ); break;
+        case NC_STRING  : fprintf( stdout, "Attribute type is NC_STRING" ); break;
+        case NC_CHAR    : fprintf( stdout, "Attribute type is NC_CHAR" ); break;
+        case NC_BYTE    : fprintf( stdout, "Attribute type is NC_BYTE" ); break;
+        case NC_UBYTE   : fprintf( stdout, "Attribute type is NC_UBYTE" ); break;
+        default         : fprintf( stdout, "Attribute type not recognized." ); break;
+    }
+    #endif
+    short att_val_short;
+    unsigned short att_val_ushort;
+    int att_val_int;
+    unsigned int att_val_uint;
+    long att_val_long;
+    unsigned long att_val_ulong;
+    float att_val_float;
+    double att_val_double;
+    switch (att_type) {
+        case NC_SHORT   : nc_get_att(ncid, var_id, attrname, &att_val_short);   attr = double(att_val_short);   break;
+        case NC_USHORT  : nc_get_att(ncid, var_id, attrname, &att_val_ushort);  attr = double(att_val_ushort);  break;
+        case NC_INT     : nc_get_att(ncid, var_id, attrname, &att_val_int);     attr = double(att_val_int);     break;
+        case NC_UINT    : nc_get_att(ncid, var_id, attrname, &att_val_uint);    attr = double(att_val_uint);    break;
+        case NC_INT64   : nc_get_att(ncid, var_id, attrname, &att_val_long);    attr = double(att_val_long);    break;
+        case NC_UINT64  : nc_get_att(ncid, var_id, attrname, &att_val_ulong);   attr = double(att_val_ulong);   break;
+        case NC_FLOAT   : nc_get_att(ncid, var_id, attrname, &att_val_float);   attr = double(att_val_float);   break;
+        case NC_DOUBLE  : nc_get_att(ncid, var_id, attrname, &att_val_double);  attr = double(att_val_double);  break;
+    }
+
+    //nc_get_att(ncid, var_id, attrname, &attr);
 
     #if DEBUG >= 1
     if (wRank == 0) {
