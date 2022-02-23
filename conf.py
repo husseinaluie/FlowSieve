55b1fdc3e8ee58448b4fc742b60ec77f81afdc7f
Benjamin Storer <storer.ben@gmail.com>
2022-02-23 16:12:00 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -5,8 +5,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 # Extract version number from version file
 with open('VERSION') as f:
     contents = f.readlines()
-contents = [ x.strip() for x in contents ]
-version_nums = [ x[14:] for x in contents ]
+version_nums = [ x.strip().split('=')[-1] for x in contents ]
 VERSION = version_nums[0] + '.' + version_nums[1] + '.' + version_nums[2]
 
 if read_the_docs_build:
