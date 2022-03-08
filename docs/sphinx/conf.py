840bf87963e3437b9740301dc691fe81136cc56c
bastorer <storer.ben@gmail.com>
2022-03-08 11:51:18 -0500

    Fixing a file link

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -6,7 +6,7 @@ html_extra_path = ['../build/html']
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 # Extract version number from version file
-with open('VERSION') as f:
+with open('../../VERSION') as f:
     contents = f.readlines()
 version_nums = [ x.strip().split('=')[-1] for x in contents ]
 VERSION = version_nums[0] + '.' + version_nums[1] + '.' + version_nums[2]
