4c90cecf03ef3cdc600dfe46a829e6c132a6f628
Benjamin A. Storer <storer.ben@gmail.com>
2023-02-02 16:09:25 -0500

    Adding in post-processing to coarse_grain_scalars
    As well as functionality for getting PE/KE conversions

--- Case_Files/coarse_grain_scalars.cpp
+++ Case_Files/coarse_grain_scalars.cpp
@@ -12,6 +12,7 @@
 #include "../netcdf_io.hpp"
 #include "../functions.hpp"
 #include "../constants.hpp"
+#include "../postprocess.hpp"
 
 int main(int argc, char *argv[]) {
     
@@ -37,6 +38,10 @@ int main(int argc, char *argv[]) {
     static_assert( not( (constants::EXTEND_DOMAIN_TO_POLES) and (constants::CARTESIAN) ),
             "Cartesian implies that there are no poles, so cannot extend to poles."
             "Please update constants.hpp accordingly.");
+
+    // Cannot have OkuboWeiss and postprocessing turned on
+    static_assert( not( (constants::DO_OKUBOWEISS_ANALYSIS) and (constants::APPLY_POSTPROCESS) ),
+           "" );
 
     // Enable all floating point exceptions but FE_INEXACT
     //feenableexcept( FE_ALL_EXCEPT & ~FE_INEXACT & ~FE_UNDERFLOW );
@@ -77,13 +82,28 @@ int main(int argc, char *argv[]) {
 
     const std::string &latlon_in_degrees  = input.getCmdOption("--is_degrees",   "true", asked_help);
 
+    const std::string &compute_PEKE_conv  = input.getCmdOption("--do_PEKE_conversion", "false", asked_help);
+
     const std::string   &Nprocs_in_time_string  = input.getCmdOption("--Nprocs_in_time",  "1", asked_help),
                         &Nprocs_in_depth_string = input.getCmdOption("--Nprocs_in_depth", "1", asked_help);
     const int   Nprocs_in_time_input  = stoi(Nprocs_in_time_string),
                 Nprocs_in_depth_input = stoi(Nprocs_in_depth_string);
 
+    const std::string   &region_defs_fname    = input.getCmdOption("--region_definitions_file",    
+                                                                   "region_definitions.nc", 
+                                                                   asked_help,
+                                                                   "netCDF file containing user-specified region definitions."),
+                        &region_defs_dim_name = input.getCmdOption("--region_definitions_dim",     
+                                                                   "region",                
+                                                                   asked_help,
+                                                                   "Name of the region dimension in the regions file."),
+                        &region_defs_var_name = input.getCmdOption("--region_definitions_var",     
+                                                                   "region_definition",     
+                                                                   asked_help,
+                                                                   "Name of the variable in the regions file that provides the region definitions.");
+
     // Also read in the fields to be filtered from commandline
-    //   e.g. --filter_scales "rho salinity p" (names must match with input netcdf file)
+    //   e.g. --variables "rho salinity p" (names must match with input netcdf file)
     std::vector< std::string > vars_to_filter;
     input.getListofStrings( vars_to_filter, "--variables", asked_help );
     const int Nvars = vars_to_filter.size();
@@ -101,6 +121,8 @@ int main(int argc, char *argv[]) {
 
     // Print some header info, depending on debug level
     print_header_info();
+
+    Timing_Records timing_records;
 
     // Initialize dataset class instance
     dataset source_data;
@@ -129,13 +151,13 @@ int main(int argc, char *argv[]) {
 
     // Compute the area of each 'cell' which will be necessary for integration
     #if DEBUG >= 2
-    fprintf( stdout, "Computing cell areas.\n" );
+    if (wRank == 0) { fprintf( stdout, "Computing cell areas.\n" ); }
     #endif
     source_data.compute_cell_areas();
 
     // Read in the scalar fields to filter
     #if DEBUG >= 1
-    fprintf( stdout, "Reading in original fields.\n" );
+    if (wRank == 0) { fprintf( stdout, "Reading in original fields.\n" ); }
     #endif
     for (size_t field_ind = 0; field_ind < vars_to_filter.size(); field_ind++) {
         source_data.load_variable( vars_to_filter.at(field_ind), vars_to_filter.at(field_ind), input_fname, true, true, !one_snapshot );
@@ -157,9 +179,22 @@ int main(int argc, char *argv[]) {
                 Nlon    = source_data.Nlon;
     size_t Ivar, index;
 
+    // Read in the region definitions and compute region areas
+    if ( check_file_existence( region_defs_fname ) ) {
+        // If the file exists, then read in from that
+        source_data.load_region_definitions( region_defs_fname, region_defs_dim_name, region_defs_var_name );
+    } else {
+        // Otherwise, just make a single region which is the entire domain
+        source_data.region_names.push_back("full_domain");
+        source_data.regions.insert( std::pair< std::string, std::vector<bool> >( 
+                                    "full_domain", std::vector<bool>( source_data.Nlat * source_data.Nlon, true) ) 
+                );
+        source_data.compute_region_areas();
+    }
+
     //
     #if DEBUG >= 1
-    fprintf( stdout, "Setting up coarse fields.\n" );
+    if (wRank == 0) { fprintf( stdout, "Setting up coarse fields.\n" ); fflush(stdout); }
     #endif
     std::vector< std::vector<double> > coarse_fields(Nvars);
     for (size_t field_ind = 0; field_ind < vars_to_filter.size(); field_ind++) {
@@ -170,13 +205,34 @@ int main(int argc, char *argv[]) {
     //// Set up filtering vectors
     //
     #if DEBUG >= 1
-    fprintf( stdout, "Setting up filtering values.\n" );
+    if (wRank == 0) { fprintf( stdout, "Setting up filtering values.\n" ); fflush(stdout); }
     #endif
     std::vector<double > filter_values_doubles, local_kernel(Nlat * Nlon, 0.);
     std::vector<double*> filter_values_ptrs;
     std::vector<const std::vector<double>*> filter_fields;
     for (size_t field_ind = 0; field_ind < vars_to_filter.size(); field_ind++) {
         filter_fields.push_back( &source_data.variables.at(vars_to_filter.at(field_ind)) );
+    }
+    
+
+    // Set up postprocessing fields
+    std::vector<const std::vector<double>*> postprocess_fields;
+    std::vector<std::string> postprocess_names;
+    int rho_ind = -1, wo_ind = -1;
+    for ( Ivar = 0; Ivar < Nvars; Ivar++ ) {
+        postprocess_fields.push_back( &coarse_fields.at(Ivar) );
+        postprocess_names.push_back( vars_to_filter.at(Ivar) );
+        if ( compute_PEKE_conv == "true" ) {
+            if (vars_to_filter.at(Ivar) == "rho") { rho_ind = Ivar; }
+            if (vars_to_filter.at(Ivar) == "wo" ) { wo_ind = Ivar; }
+        }
+    }
+    if ( compute_PEKE_conv == "true" ) {
+        postprocess_names.push_back( "barrho_barwo" );
+        std::vector<double> barrho_barwo( filter_fields[0]->size(), 0. );
+        postprocess_fields.push_back( &barrho_barwo );
+        assert( rho_ind >= 0 );
+        assert( wo_ind >= 0 );
     }
 
     //
@@ -187,12 +243,13 @@ int main(int argc, char *argv[]) {
         double scale = filter_scales.at(ell_ind);
 
         #if DEBUG >= 0
-        fprintf( stdout, "Filter scale %'g km.\n", scale / 1e3 );
+        if (wRank == 0) { fprintf( stdout, "Filter scale %'g km.\n", scale / 1e3 ); }
         #endif
 
         #pragma omp parallel \
         default(none) \
-        shared( source_data, filter_fields, coarse_fields, scale, stdout ) \
+        shared( source_data, filter_fields, coarse_fields, scale, stdout, \
+              barrho_barwo, rho_ind, wo_ind ) \
         private( filter_values_doubles, filter_values_ptrs, \
                  Itime, Idepth, Ilat, Ilon, Ivar, index, \
                  LAT_lb, LAT_ub ) \
@@ -239,6 +296,8 @@ int main(int argc, char *argv[]) {
                             for ( Ivar = 0; Ivar < Nvars; Ivar++ ) {
                                 coarse_fields.at(Ivar).at(index) = filter_values_doubles.at(Ivar);
                             }
+
+                            barrho_barwo.at(index) = coarse_fields.at(rho_ind).at(index) * coarse_fields.at(wo_ind).at(index);
                         }
                     }
                 }
@@ -248,33 +307,59 @@ int main(int argc, char *argv[]) {
         //
         //// Create output file
         //
+        if (not(constants::NO_FULL_OUTPUTS)) {
+            char fname [50];
+            snprintf(fname, 50, "filter_%.6gkm.nc", scale / 1e3);
+            initialize_output_file( source_data, vars_to_filter, fname, scale );
 
-        char fname [50];
-        snprintf(fname, 50, "filter_%.6gkm.nc", scale / 1e3);
-        initialize_output_file( source_data, vars_to_filter, fname, scale );
+            const int ndims = 4;
+            size_t starts[ndims];
+            if (one_snapshot) {
+                starts[0] = 0;
+                starts[1] = 0;
+                starts[2] = size_t(myStarts.at(0));
+                starts[3] = size_t(myStarts.at(1));
+            } else {
+                starts[0] = size_t(myStarts.at(0));
+                starts[1] = size_t(myStarts.at(1));
+                starts[2] = size_t(myStarts.at(2));
+                starts[3] = size_t(myStarts.at(3));
+            }
+            size_t counts[ndims] = { size_t(Ntime),          size_t(Ndepth),         size_t(Nlat),           size_t(Nlon)           };
 
-        const int ndims = 4;
-        size_t starts[ndims];
-        if (one_snapshot) {
-            starts[0] = 0;
-            starts[1] = 0;
-            starts[2] = size_t(myStarts.at(0));
-            starts[3] = size_t(myStarts.at(1));
-        } else {
-            starts[0] = size_t(myStarts.at(0));
-            starts[1] = size_t(myStarts.at(1));
-            starts[2] = size_t(myStarts.at(2));
-            starts[3] = size_t(myStarts.at(3));
+            for ( Ivar = 0; Ivar < Nvars; Ivar++ ) {
+                write_field_to_output( coarse_fields.at(Ivar), vars_to_filter.at(Ivar), starts, counts, fname, &source_data.mask );
+            }
         }
-        size_t counts[ndims] = { size_t(Ntime),          size_t(Ndepth),         size_t(Nlat),           size_t(Nlon)           };
 
-        for ( Ivar = 0; Ivar < Nvars; Ivar++ ) {
-            write_field_to_output( coarse_fields.at(Ivar), vars_to_filter.at(Ivar), starts, counts, fname, &source_data.mask );
+
+        //
+        //// on-line postprocessing, if desired
+        //
+
+        if (constants::APPLY_POSTPROCESS) {
+            MPI_Barrier(MPI_COMM_WORLD);
+
+            #if DEBUG >= 1
+            if (wRank == 0) { fprintf(stdout, "Beginning post-process routines\n"); }
+            fflush(stdout);
+            #endif
+
+            std::vector<double> null_vector(0);
+            Apply_Postprocess_Routines(
+                    source_data, postprocess_fields, postprocess_names, null_vector,
+                    scale, timing_records);
+
+            #if DEBUG >= 1
+            if (wRank == 0) { fprintf(stdout, "Finished post-process routines\n"); }
+            fflush(stdout);
+            #endif
         }
-    }
+
+    } // end ell loop
 
 
-
+    // DONE!
     #if DEBUG >= 1
     fprintf(stdout, "Processor %d / %d waiting to finalize.\n", wRank + 1, wSize);
     #endif
