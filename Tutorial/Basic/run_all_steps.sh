a97a5267a0425337048ffde33dec98a47c06315d
Benjamin A. Storer <storer.ben@gmail.com>
2023-02-02 12:52:40 -0500

    Even with serial, using mpirun -n 1 seems to sidestep some gcc errors

--- Tutorial/Basic/run_all_steps.sh
+++ Tutorial/Basic/run_all_steps.sh
@@ -10,7 +10,7 @@ python generate_data.py
 ## Run coarse-graining
 ##
 
-./coarse_grain.x --input_file velocity_sample.nc --filter_scales "1e3 15e3 50e3 100e3"
+mpirun -n 1 ./coarse_grain.x --input_file velocity_sample.nc --filter_scales "1e3 15e3 50e3 100e3"
 
 ##
 ## Make figures from the outputs
