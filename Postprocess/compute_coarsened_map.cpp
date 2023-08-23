4c6e1592143a50b9d0f6ffc9b06bbe123cd2ac37
Benjamin Storer <storer.ben@gmail.com>
2023-08-23 12:01:28 -0500

    Updating the coarsened maps to use correct area weighting.
    Coarsened maps are now online!

--- Postprocess/compute_coarsened_map.cpp
+++ Postprocess/compute_coarsened_map.cpp
@@ -31,11 +31,60 @@ void compute_coarsened_map(
 
     const int   num_fields = postprocess_fields.size();
 
-    double dA, dA_coarse;
+    double dA, dA_coarse, increment;
 
     int Ifield, Itime, Idepth, Ilat, Ilon, Ilat_coarse, Ilon_coarse,
         lat_LB, lat_UB, lon_LB, lon_UB;
     size_t coarse_index, index;
+
+    std::vector<double> coarsened_areas( postprocess_fields[0]->size(), 0. );
+    #if DEBUG >= 1
+    if (wRank == 0) { fprintf(stdout, "  Computing coarsened areas\n"); }
+    fflush(stdout);
+    #endif
+
+    #pragma omp parallel default(none)\
+    private(Ilat, Ilon, index, coarse_index, dA, \
+            Idepth, Itime, lat_LB, lat_UB, lon_LB, lon_UB )\
+    shared( source_data, latitude, longitude, coarse_latitude, coarse_longitude, coarsened_areas ) \
+    firstprivate( Nlon, Nlat, Ndepth, Ntime, Nlat_coarse, Nlon_coarse )
+    { 
+        #pragma omp for collapse(4) schedule(static)
+        for (Itime = 0; Itime < Ntime; ++Itime) {
+            for (Idepth = 0; Idepth < Ndepth; ++Idepth) {
+                for (Ilat_coarse = 0; Ilat_coarse < Nlat_coarse; ++Ilat_coarse) {
+                    for (Ilon_coarse = 0; Ilon_coarse < Nlon_coarse; ++Ilon_coarse) {
+
+                        lat_LB = std::lower_bound( latitude.begin(), latitude.end(), coarse_latitude.at(Ilat_coarse) ) - latitude.begin();
+                        lat_UB = ( Ilat_coarse + 1 < Nlat_coarse )
+                                 ? ( std::lower_bound( latitude.begin(), latitude.end(), coarse_latitude.at(Ilat_coarse+1) ) ) - latitude.begin()
+                                 : Nlat - 1;
+
+                        lat_LB = ( lat_LB < 0 ) ? 0 : ( lat_LB >= Nlat ) ? Nlat - 1 : lat_LB;
+                        lat_UB = ( lat_UB < 0 ) ? 0 : ( lat_UB >= Nlat ) ? Nlat - 1 : lat_UB;
+
+                        lon_LB = std::lower_bound( longitude.begin(), longitude.end(), coarse_longitude.at(Ilon_coarse) ) - longitude.begin();
+                        lon_UB = ( Ilon_coarse + 1 < Nlon_coarse )
+                                 ? ( std::lower_bound( longitude.begin(), longitude.end(), coarse_longitude.at(Ilon_coarse+1) ) ) - longitude.begin()
+                                 : Nlon - 1;
+
+                        lon_LB = ( lon_LB < 0 ) ? 0 : ( lon_LB >= Nlon ) ? Nlon - 1 : lon_LB;
+                        lon_UB = ( lon_UB < 0 ) ? 0 : ( lon_UB >= Nlon ) ? Nlon - 1 : lon_UB;
+
+                        coarse_index = Index(Itime, Idepth, Ilat_coarse, Ilon_coarse, Ntime, Ndepth, Nlat_coarse, Nlon_coarse);
+
+                        for ( Ilat = lat_LB; Ilat < lat_UB; Ilat++ ) {
+                            for ( Ilon = lon_LB; Ilon < lon_UB; Ilon++ ) {
+                                index = Index(0, 0, Ilat, Ilon, Ntime, Ndepth, Nlat, Nlon);
+                                dA = source_data.areas.at(index);
+                                coarsened_areas.at(coarse_index) += dA;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     #if DEBUG >= 1
     int wRank;
@@ -53,10 +102,10 @@ void compute_coarsened_map(
         #endif
 
         #pragma omp parallel default(none)\
-        private(Ilat, Ilon, index, coarse_index, dA, dA_coarse, \
+        private(Ilat, Ilon, index, coarse_index, dA, dA_coarse, increment, \
                 Idepth, Itime, lat_LB, lat_UB, lon_LB, lon_UB )\
         shared( source_data, Ifield, coarsened_maps, postprocess_fields, latitude, longitude, \
-                coarse_latitude, coarse_longitude ) \
+                coarse_latitude, coarse_longitude, coarsened_areas ) \
         firstprivate( Nlon, Nlat, Ndepth, Ntime, Nlat_coarse, Nlon_coarse )
         { 
             #pragma omp for collapse(4) schedule(static)
@@ -81,9 +130,8 @@ void compute_coarsened_map(
                             lon_LB = ( lon_LB < 0 ) ? 0 : ( lon_LB >= Nlon ) ? Nlon - 1 : lon_LB;
                             lon_UB = ( lon_UB < 0 ) ? 0 : ( lon_UB >= Nlon ) ? Nlon - 1 : lon_UB;
 
-                            coarse_index = Index(0, 0, Ilat_coarse, Ilon_coarse, Ntime, Ndepth, Nlat_coarse, Nlon_coarse);
-                            dA = source_data.coarse_map_areas.at(coarse_index);
                             coarse_index = Index(Itime, Idepth, Ilat_coarse, Ilon_coarse, Ntime, Ndepth, Nlat_coarse, Nlon_coarse);
+                            dA_coarse = coarsened_areas.at(coarse_index);
 
                             for ( Ilat = lat_LB; Ilat < lat_UB; Ilat++ ) {
                                 for ( Ilon = lon_LB; Ilon < lon_UB; Ilon++ ) {
@@ -92,9 +140,9 @@ void compute_coarsened_map(
 
                                     index = Index(Itime, Idepth, Ilat, Ilon, Ntime, Ndepth, Nlat, Nlon);
 
-                                    coarsened_maps.at(Ifield).at( coarse_index ) = (dA_coarse > 0) ? 
-                                                                                        postprocess_fields.at(Ifield)->at(index) * dA / dA_coarse
-                                                                                        : 0.;
+                                    increment = (dA_coarse > 0) ? (postprocess_fields.at(Ifield)->at(index) * dA / dA_coarse) : 0.;
+
+                                    coarsened_maps.at(Ifield).at( coarse_index ) += increment;
                                 }
                             }
                         }
