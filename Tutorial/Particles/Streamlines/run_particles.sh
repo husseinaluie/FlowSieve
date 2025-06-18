348bf88f9fbc4d7796b6f4267daa9eb1c2ee29d2
Benjamin Storer <storer.ben@gmail.com>
2025-06-18 08:26:02 -0500

    Updating streamline tutorial a bit

--- Tutorial/Particles/Streamlines/run_particles.sh
+++ Tutorial/Particles/Streamlines/run_particles.sh
@@ -1,14 +1,16 @@
-FINAL_TIME="8760" # one year in hours [unit of 'time_unit']
+FINAL_TIME="26280" # three years in hours [unit of 'time_unit']
 OUTPUT_FREQUENCY="28800" # eight hours in seconds [always in seconds]
 
-NUM_PARTICLES=1000  # this is the number of particles *per MPI rank*
+NUM_PARTICLES="1000"  # this is the number of particles *per MPI rank*
+
+set -ex
 
 ./particles.x --zonal_vel "uo" \
               --merid_vel "vo" \
               --input_file "velocity_sample.nc" \
               --output_file "particles.nc" \
               --time_unit "hours" \
-              --particles_per_mpi ${NUM_PARTICLE} \
-              --output_frequency ${OUTPUT_FREQUENCE} \
+              --particle_per_mpi ${NUM_PARTICLES} \
+              --output_frequency ${OUTPUT_FREQUENCY} \
               --final_time ${FINAL_TIME} \
               --particle_lifespan -1
--- Tutorial/Particles/Streamlines/submit.sh
+++ Tutorial/Particles/Streamlines/submit.sh
@@ -14,7 +14,7 @@ export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export KMP_AFFINITY="compact"
 export I_MPI_PIN_DOMAIN="auto"
 
-FINAL_TIME="8760" # one year in hours [unit of 'time_unit']
+FINAL_TIME="26280" # three years in hours [unit of 'time_unit']
 OUTPUT_FREQUENCY="28800" # eight hours in seconds [always in seconds]
 
 NUM_PARTICLES=1000  # this is the number of particles *per MPI rank*
@@ -24,7 +24,7 @@ NUM_PARTICLES=1000  # this is the number of particles *per MPI rank*
               --input_file "velocity_sample.nc" \
               --output_file "particles.nc" \
               --time_unit "hours" \
-              --particles_per_mpi ${NUM_PARTICLE} \
+              --particle_per_mpi ${NUM_PARTICLES} \
               --output_frequency ${OUTPUT_FREQUENCY} \
               --final_time ${FINAL_TIME} \
               --particle_lifespan -1
